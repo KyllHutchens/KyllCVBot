ea5d7d36383b566e927aa8faac8934725c76d05a
KyllHutchens <kyll.hutchens0@gmail.com>
2023-07-18 15:42:15 +1000

    Take 3

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, url_for, flash, session, jsonify, send_from_directory
 import os
-import secrets
+
 from ChatAI import cv_chat
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
@@ -8,13 +8,6 @@ app.config['JSONIFY_MIMETYPE'] = 'application/json'
 
 
 # Create a cursor object
-
-secret_keyss = secrets.token_hex(16)
-app.secret_key = secret_keyss
-
-
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'  # Enable non-HTTPS for development
-
 @app.route('/api/set_timezone', methods=['POST'])
 def set_timezone():
     print('Received a request.')
@@ -46,6 +39,5 @@ def catch_all(path):
         response.headers['Expires'] = '0'
         return response
 
-if __name__ == '__main__':
-
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
